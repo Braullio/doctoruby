65b2fbcdfcb9389bc996a5ea67187ae71d261deb
braullio <braulliovg@gmail.com>
2020-08-28 12:37:46 -0300

    accept crm only number

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -3,6 +3,6 @@
 # Interaction table doctor
 class Doctor < ApplicationRecord
   validates :name, presence: true
-  validates :crm, presence: true
+  validates :crm, presence: true, numericality: { only_integer: true }
   validates :crm_uf, presence: true
 end
