8f34711be733f11c7ea57be1432cc6613b829468
braullio <braulliovg@gmail.com>
2020-08-28 01:07:03 -0300

    create doctor controller

---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+# Controller to Doctors
+class DoctorsController < ApplicationController
+  before_action :doctor, only: %i[show edit update destroy]
+
+  def index
+    @doctors = Doctor.all
+  end
+
+  def show; end
+
+  def new
+    @doctor = Doctor.new
+  end
+
+  def edit; end
+
+  def create
+    @doctor = Doctor.new(doctor_params)
+
+    respond_to do |format|
+      if @doctor.save
+        format.html { redirect_to doctors_path, notice: 'Doutor criado com sucesso!' }
+      else
+        format.html { redirect_to doctors_path, alert: @doctor.errors.full_messages.to_sentence }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @doctor.update(doctor_params)
+        format.html { redirect_to doctors_path, notice: 'Doutor atualizado com sucesso!' }
+      else
+        format.html { redirect_to doctors_path, alert: @doctor.errors.full_messages.to_sentence }
+      end
+    end
+  end
+
+  def destroy
+    respond_to do |format|
+      if @doctor.destroy
+        format.html { redirect_to doctors_url, notice: 'Doutor deletado com sucesso!' }
+      else
+        format.html { redirect_to doctors_path, alert: @doctor.errors.full_messages.to_sentence }
+      end
+    end
+  end
+
+  private
+
+  def doctor
+    @doctor = Doctor.find(params[:id])
+  end
+
+  def doctor_params
+    params.require(:doctor).permit(:name, :crm, :crm_uf)
+  end
+end
