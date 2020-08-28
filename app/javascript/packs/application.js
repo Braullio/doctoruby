e8f504566f36a3984b1b417a7601cf5dd46205f8
braullio <braulliovg@gmail.com>
2020-08-28 01:28:43 -0300

    change the extension from css to scss

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,6 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import '../src/application.css';
+import '../src/application.scss';
 import './flashMessages';
 import 'bootstrap';
