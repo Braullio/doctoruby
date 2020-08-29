a17812496a97c4cf8f52159abc160010e99bc899
braullio <braulliovg@gmail.com>
2020-08-28 22:20:38 -0300

    add patient model

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,6 +2,8 @@
 
 # Interaction table doctor
 class Doctor < ApplicationRecord
+  has_many :patients
+
   validates :name, presence: true
   validates :crm, presence: true, numericality: { only_integer: true }
   validates :crm_uf, presence: true
