429f74668c567c50a39faf8928074f5a3d71be6f
braullio <braulliovg@gmail.com>
2020-08-29 13:26:50 -0300

    not delete doctor if have patients

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,7 +2,7 @@
 
 # Interaction table doctor
 class Doctor < ApplicationRecord
-  has_many :patients
+  has_many :patients, dependent: :restrict_with_error
 
   validates :name, presence: true
   validates :crm, presence: true, numericality: { only_integer: true }
