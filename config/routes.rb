77e0e2fe3cb5ad701fd8ee77676354fd8fe26222
braullio <braulliovg@gmail.com>
2020-08-29 21:16:27 -0300

    update routes appointment

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   root 'home#index'
   resources :doctors, only: %i[create update destroy]
   resources :patients, only: %i[create update destroy]
-  resources :appointments
+  resources :appointments, only: %i[index create update destroy]
 end
