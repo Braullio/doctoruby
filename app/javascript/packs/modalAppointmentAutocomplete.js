3aa4b9deb51d412e4f18181984eef873fc599ce2
braullio <braulliovg@gmail.com>
2020-08-30 02:26:52 -0300

    add modal_appointment_edit

--- app/javascript/packs/modalAppointmentAutocomplete.js
+++ app/javascript/packs/modalAppointmentAutocomplete.js
@@ -1,7 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
   $('#modal_appointment_new').on('show.bs.modal', function (e) {
-    var hour = $(e.relatedTarget).data('hora');
+    var hour = $(e.relatedTarget).data('hour');
     var min = $(e.relatedTarget).data('min');
-    $(e.currentTarget).find('input[name="hour"]').val(String(hour).concat(':', String(min)));
+    document.getElementById('hour').value = String(hour).concat(':', String(min));
+  })
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+  $('#modal_appointment_edit').on('show.bs.modal', function (e) {
+    var doctorId = $(e.relatedTarget).data('id');
+    $(e.currentTarget).find('input[name="appointment[id]"]').val(doctorId);
+
+    var hour = $(e.relatedTarget).data('hour');
+    var min = $(e.relatedTarget).data('min');
+    document.getElementById('hour_edit').value = String(hour).concat(':', String(min));
+
+    var patientId = $(e.relatedTarget).data('patient');
+    document.getElementById('Patient').value = patientId;
   })
 })
