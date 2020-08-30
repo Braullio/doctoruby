1f0cc30e7295ff5eb4c5560a53ab272a18060263
braullio <braulliovg@gmail.com>
2020-08-29 23:36:59 -0300

    update controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -7,6 +7,9 @@ class AppointmentsController < ApplicationController
   def index
     @appointments = Appointment.all
     @appointment = Appointment.new
+    @doctor_appointment = Doctor.find(17)
+    @patients_appointment = @doctor_appointment.patients
+    @date = Date.today - 2.day
   end
 
   def create
@@ -14,9 +17,9 @@ class AppointmentsController < ApplicationController
 
     respond_to do |format|
       if @appointment.save
-        format.html { redirect_to @appointment, notice: 'Agendamento criado com sucesso!' }
+        format.html { redirect_to appointments_url, notice: 'Agendamento criado com sucesso!' }
       else
-        format.html { redirect_to appointments_url, alert: @doctor.errors.full_messages.to_sentence }
+        format.html { redirect_to appointments_url, alert: @appointment.errors.full_messages.to_sentence }
       end
     end
   end
@@ -24,9 +27,9 @@ class AppointmentsController < ApplicationController
   def update
     respond_to do |format|
       if @appointment.update(appointment_params)
-        format.html { redirect_to @appointment, notice: 'Appointment atualizado com sucesso!' }
+        format.html { redirect_to appointments_url, notice: 'Appointment atualizado com sucesso!' }
       else
-        format.html { redirect_to appointments_url, alert: @doctor.errors.full_messages.to_sentence }
+        format.html { redirect_to appointments_url, alert: @appointment.errors.full_messages.to_sentence }
       end
     end
   end
@@ -34,10 +37,10 @@ class AppointmentsController < ApplicationController
   def destroy
     @appointment.destroy
     respond_to do |format|
-      if @doctor.destroy
+      if @appointment.destroy
         format.html { redirect_to appointments_url, notice: 'Agendamento deletado com sucesso!' }
       else
-        format.html { redirect_to appointments_url, alert: @doctor.errors.full_messages.to_sentence }
+        format.html { redirect_to appointments_url, alert: @appointment.errors.full_messages.to_sentence }
       end
     end
   end
@@ -49,6 +52,20 @@ class AppointmentsController < ApplicationController
   end
 
   def appointment_params
+    merge_start_ends
     params.require(:appointment).permit(:starts_at, :ends_at, :doctor_id, :patient_id)
+  end
+
+  def merge_start_ends
+    merge_starts
+    merge_ends
+  end
+
+  def merge_starts
+    params['appointment'].merge!(starts_at: (params['day'] + ' ' + params['hour']).to_time)
+  end
+
+  def merge_ends
+    params['appointment'].merge!(ends_at: (params['day'] + ' ' + params['hour']).to_time + 1800)
   end
 end
