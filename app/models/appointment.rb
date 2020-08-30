85a947fb5fd2d527de4171f7ad122aade04fe9f2
braullio <braulliovg@gmail.com>
2020-08-29 21:01:11 -0300

    add validations

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,15 @@
+# frozen_string_literal: true
+
+# Interaction table Appointment
 class Appointment < ApplicationRecord
   belongs_to :doctor
   belongs_to :patient
+
+  validates :patient_id, presence: true
+  validates :doctor_id, presence: true
+  validates :starts_at, presence: true
+  validates :ends_at, presence: true
+
+  validates :starts_at, uniqueness: { scope: :doctor_id, message: 'não pode repetir o horário!' }
+  validates :ends_at, uniqueness: { scope: :doctor_id, message: 'não pode repetir o horário!' }
 end
