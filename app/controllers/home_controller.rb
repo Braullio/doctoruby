b427d892388bab82cf26019aed767063038d5519
braullio <braulliovg@gmail.com>
2020-08-30 04:16:25 -0300

    set values to search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,21 @@
 
 # Controller to Doctors
 class HomeController < ApplicationController
-  def index; end
+  def index
+    search = params['doctor_id'].blank? ? search_doctor_nil : search_doctor_present
+
+    @quantity_appointments = search.size
+    @quantity_patients     = search.pluck(:patient_id).uniq.size
+    @doctor_dashboard_id   = params['doctor_id']
+  end
+
+  private
+
+  def search_doctor_nil
+    Appointment.where("starts_at > '#{Time.now}'")
+  end
+
+  def search_doctor_present
+    Appointment.where("starts_at > '%#{Time.now}%' and doctor_id = #{params['doctor_id']}")
+  end
 end
